c6a04530d095f59d60d59bacd3c5a0938ce757ee
daviderigamonti <davide2.rigamonti@mail.polimi.it>
2024-09-14 11:42:00 +0200

    Fixed __init__ fiel

--- src/transformer_wrappers/wrappers/__init__.py
+++ src/transformer_wrappers/wrappers/__init__.py
@@ -3,7 +3,7 @@ from .parallel import ParallelTransformerWrapper, ParallelCausalLMWrapper
 from .resizable import ResizableTokenizer, ResizableTransformerWrapper, ResizableCausalLMWrapper
 from .circuit import CircuitCausalLMWrapper
 from .parameter import ParameterCausalLMWrapper
-from .ablation import AblationCausalLMWrapper
+from .ablation import AblationCausalLMWrapper, AblationInfo, AblationPosition
 from .inject import AblInjCausalLMWrapper, InjectCausalLMWrapper, InjectInfo, InjectPosition
 
 from .utils import transformer_mapping, causal_lm_mapping
